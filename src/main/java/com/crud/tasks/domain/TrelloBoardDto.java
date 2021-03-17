b5add0d54be42b76913c680de8c68c167ee43f3d
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2021-03-17 18:13:02 +0100

    23.3 - send mail

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloBoardDto {
